4d75c9b6494d5d8ef14145536fc696ea28b66abb
Jelena <jeca_997@hotmail.com>
2020-04-26 13:36:07 +0200

    finished book review

--- Frontend/src/app/book-review-form/book-review-form.component.ts
+++ Frontend/src/app/book-review-form/book-review-form.component.ts
@@ -6,8 +6,9 @@ import { Component, OnInit, Renderer2, ViewChild, ElementRef } from '@angular/co
   styleUrls: ['./book-review-form.component.css']
 })
 export class BookReviewFormComponent implements OnInit {
-  @ViewChild('generatedTags') div: ElementRef;
-  @ViewChild('generatedTags') content: ElementRef;
+  @ViewChild('content') content: ElementRef;
+  @ViewChild('actionButtons') actionButtons: ElementRef;
+  @ViewChild('form') form: ElementRef;
 
   constructor(private renderer: Renderer2) { }
 
@@ -15,33 +16,30 @@ export class BookReviewFormComponent implements OnInit {
   }
 
   generateTagInfo(event: Event) {
-    /*  <div class="col-6 col-12-xsmall">
-                            <input type="text" name="prezime" id="prezime" value="" placeholder="Prezime">
-                        </div>
-                        <div class="col-6 col-12-xsmall">
-                            <input type="text" name="username" id="username" value="" placeholder="Username">
-                        </div>*/
-                        
-    //const div0: HTMLParagraphElement = this.renderer.createElement('div');
-    const div0: HTMLParagraphElement = this.div.nativeElement;
-   
-    this.renderer.insertBefore()
-    //this.renderer.addClass(div0,"col-12");
-    //this.renderer.addClass(div0,"col-12-xsmall");
+    const div1: HTMLDivElement = this.renderer.createElement('div');
+    div1.innerHTML = "<input type=\"text\" name=\"tag\" id=\"tag\" value=\"\" placeholder=\"Tag\">";
+    this.renderer.addClass(div1,"col-6");
+    this.renderer.addClass(div1,"col-12-xsmall");
+    this.renderer.addClass(div1,"generated");
+    const div2: HTMLDivElement = this.renderer.createElement('div');
+    div2.innerHTML = "<input type=\"text\" name=\"prezime\" id=\"info\" value=\"\" placeholder=\"Informacija\">";
+    this.renderer.addClass(div2,"col-6");
+    this.renderer.addClass(div2,"col-12-xsmall");
+    this.renderer.addClass(div2,"generated");
 
-    //const div1: HTMLDivElement = this.renderer.createElement('div');
-    //div1.innerHTML = "<input type=\"text\" name=\"tag\" id=\"tag\" value=\"\" placeholder=\"Tag\">";
-    //this.renderer.addClass(div1,"col-6");
-    //this.renderer.addClass(div1,"col-12-xsmall");
-    //const div2: HTMLDivElement = this.renderer.createElement('div');
-    //div2.innerHTML = "<input type=\"text\" name=\"prezime\" id=\"info\" value=\"\" placeholder=\"Informacija\">";
-    //this.renderer.addClass(div2,"col-6");
-    //this.renderer.addClass(div2,"col-12-xsmall");
+    this.renderer.insertBefore(this.content.nativeElement, div1, this.actionButtons.nativeElement);
+    this.renderer.insertBefore(this.content.nativeElement, div2, this.actionButtons.nativeElement);
+  }
+
+  reset(event:Event){
+
+    event.preventDefault();
     
+    this.form.nativeElement.reset();
 
-    //this.renderer.appendChild(div0, div1)
-    //this.renderer.appendChild(div0, div2)
-
-
+    let generatedElements = this.content.nativeElement.querySelectorAll('.generated');
+    for(let el of generatedElements){
+      el.remove();
+    }
   }
 }
