0283dbc3fbc2ce9d2ec58da22a9d19208436ad12
TLazarevic <lazarevict97@gmail.com>
2020-06-08 23:27:06 +0200

    updated most of the virtual assistant rules based on suggestions from checkpoint 1. No if else statements in almost all rules and almost no iterating

---
+++ Backend/drools-spring-app/src/main/java/sbnz/integracija/example/facts/TagNo.java
@@ -0,0 +1,22 @@
+package sbnz.integracija.example.facts;
+
+public class TagNo {
+	
+	public int tagNo;
+
+	public TagNo(int tagNo) {
+		super();
+		this.tagNo = tagNo;
+	}
+
+	public int getTagNo() {
+		return tagNo;
+	}
+
+	public void setTagNo(int tagNo) {
+		this.tagNo = tagNo;
+	}
+
+	public TagNo() {
+	}
+}
